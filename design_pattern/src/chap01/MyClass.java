0e1f9682f1d0737cadb6390a26c1583feb6b7803
davemins <davemins4125@naver.com>
2023-09-25 09:53:23 +0900

    feat : 클래스 추가 : Audio.java, MyClass.java, MyClassExample.java, RemoteControl.java, Television.java

---
+++ design_pattern/src/chap01/MyClass.java
@@ -0,0 +1,30 @@
+package chap01;
+
+public class MyClass {
+
+    // 필드
+    RemoteControl rc = new Television();
+
+    // 생성자
+    MyClass () {
+
+    }
+
+    MyClass (RemoteControl rc) {
+        this.rc = rc;
+        rc.turnOff();
+        rc.setVolume(5);
+    }
+
+    // 메소드
+    void methodA() {
+        RemoteControl rc = new Audio();
+        rc.turnOn();
+        rc.setVolume(5);
+    }
+
+    void methodB(RemoteControl rc) {
+        rc.turnOn();
+        rc.setVolume(5);
+    }
+}
