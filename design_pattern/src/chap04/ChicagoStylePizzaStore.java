fb84b41323acce54d6fd7b164a99e39b9659ddfd
davemins <davemins4125@naver.com>
2023-10-02 12:02:54 +0900

    docs : 구상 제품 클래스 추가 : ChicagoStyleCheesePizza.java

--- design_pattern/src/chap04/ChicagoStylePizzaStore.java
+++ design_pattern/src/chap04/ChicagoStylePizzaStore.java
@@ -2,7 +2,7 @@ package chap04;
 
 public class ChicagoStylePizzaStore extends PizzaStore {
 
-    Pizza createPizza(String item) {
+    public Pizza createPizza(String item) {
         if (item.equals("cheese")) {
             return new ChicagoStyleCheesePizza();
         } else if (item.equals("veggie")) {
--- design_pattern/src/chap04/NYStylePizzaStore.java
+++ design_pattern/src/chap04/NYStylePizzaStore.java
@@ -2,7 +2,7 @@ package chap04;
 
 public class NYStylePizzaStore extends PizzaStore {
 
-    Pizza createPizza(String item) {
+    public Pizza createPizza(String item) {
         if (item.equals("cheese")) {
             return new NYStyleCheesePizza();
         } else if (item.equals("veggie")) {
