e1a289396fde97167c5042d44dbbc5097851eeab
davemins <davemins4125@naver.com>
2023-10-18 17:00:53 +0900

    feat : Waitress 생성

---
+++ design_pattern/src/chap09_Iterator_Composite/Composite/Waitress.java
@@ -0,0 +1,13 @@
+package chap09_Iterator_Composite.Composite;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
