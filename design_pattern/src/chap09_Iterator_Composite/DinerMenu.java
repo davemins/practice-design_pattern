9ec8f1bf708e7b32525fedcc4d6e739339adf96b
davemins <davemins4125@naver.com>
2023-10-18 16:20:00 +0900

    test : 테스트 코드 수정

--- design_pattern/src/chap09_Iterator_Composite/DinerMenu.java
+++ design_pattern/src/chap09_Iterator_Composite/DinerMenu.java
@@ -1,6 +1,6 @@
 package chap09_Iterator_Composite;
 
-public class DinerMenu {
+public class DinerMenu implements Menu{
     static final int MAX_ITEMS = 6;
     int numberOfItems = 0;
     MenuItem[] menuItems;
