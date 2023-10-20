56cac2e1b9dab682dffddbfa588794ffa79bc03a
davemins <davemins4125@naver.com>
2023-10-20 14:03:06 +0900

    feat : 컴포지트 패턴 사용 : 오리 객체 관리 한번에

---
+++ design_pattern/src/chap12_Compound/Flock.java
@@ -0,0 +1,34 @@
+package chap12_Compound;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Flock implements Quackable {
+    List<Quackable> quackers = new ArrayList<Quackable>();
+
+    public void add(Quackable quacker) {
+        quackers.add(quacker);
+    }
+
+    @Override
+    public void quack() {
+        Iterator<Quackable> iterator = quackers.iterator();
+        while (iterator.hasNext()) {
+            Quackable quacker = iterator.next();
+            quacker.quack();
+            System.out.println();
+        }
+    }
+
+    public void registerObserver(Observer observer) {
+        Iterator<Quackable> iterator = quackers.iterator();
+        while (iterator.hasNext()) {
+            Quackable duck = (Quackable)iterator.next();
+            duck.registerObserver(observer);
+        }
+    }
+
+    public void notifyObservers() { }
+
+}
