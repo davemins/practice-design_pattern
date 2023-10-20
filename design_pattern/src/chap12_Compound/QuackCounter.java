ed09c0ff3759f9798923faf928e887c37c7a44ff
davemins <davemins4125@naver.com>
2023-10-20 14:01:39 +0900

    feat : 데코레이터 패턴 사용 : 오리가 소리를 낸 횟수

---
+++ design_pattern/src/chap12_Compound/QuackCounter.java
@@ -0,0 +1,29 @@
+package chap12_Compound;
+
+public class QuackCounter implements Quackable {
+    Quackable duck;
+    static int numberOfQuacks;
+
+    public QuackCounter (Quackable duck) {
+        this.duck = duck;
+    }
+
+    public void quack() {
+        duck.quack();
+        numberOfQuacks++;
+    }
+
+    public static int getQuacks() {
+        return numberOfQuacks;
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        duck.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        duck.notifyObservers();
+    }
+}
